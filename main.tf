7c16e7735ddd9cfdfd9df4f88627e15816ce338f
andrew elemoso <aelemoso@gmail.com>
2025-05-27 17:07:46 +0000

    commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.43.0"
+    }
+  }
+
+  cloud {
+    organization = "dr3wlab"
+
+    workspaces {
+      name = "handson"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "813-85b2f083-hands-on-with-terraform-on-azure"
+  location = "South Central US"
+}
+
+module "securestorage2" {
+  source               = "app.terraform.io/dr3wlab/securestorage2/azurerm"
+  version              = "1.0.1"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "stgshsdkfunique"
+}
